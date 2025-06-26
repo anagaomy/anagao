936a53b27d62346a3de1d1bf388880107224f98a
Ana Gao <gaomy@uci.edu>
2025-06-26 02:58:08 -0700

    modify involvement HTML and add involvement javascript

--- js/script.js
+++ js/script.js
@@ -55,8 +55,34 @@ const skills = [
 skills.forEach(skill => {
   const skillDiv = document.createElement('div');
   skillDiv.className = 'btn btn-light rounded-5 mt-md-2 disabled';
-  skillDiv.style.cssText = 'font-size: 17px; border-width: 0; margin-right: 5px';
+  skillDiv.style.cssText = 'font-size: 16px; border-width: 0; margin-right: 5px; background-color: rgba(255, 255, 255, 0.5);';
   skillDiv.textContent = skill;
 
   skillContainer.appendChild(skillDiv);
+});
+
+
+// involvement skill lists
+const skillSets = [
+  // CU
+  ["Content creation", "Design", "Branding", "Team collaboration", "Social media marketing"],
+  // CUCS
+  ["Peer support", "Collaboration", "Technical skills", "Networking", "Community engagement"], 
+  // film academy
+  ["Filming", "Directing", "script writing", "Video editing", "Storyboarding"],
+  // cyberforward
+  ["Third-party risk", "Vulnerability analysis", "Security policy"]
+];
+
+skillSets.forEach((skills, index) => {
+  const cardListContainer = document.querySelector(`.card-list-${index + 1}`);
+
+  skills.forEach(skill => {
+    const card = document.createElement('a');
+    card.className = "btn btn-light rounded-5 mt-md-2 disabled";
+    card.style = "font-size: 14px; border-width: 0; margin: 0 2px 1px 2px";
+    card.textContent = skill;
+
+    cardListContainer.appendChild(card);
+  });
 });
